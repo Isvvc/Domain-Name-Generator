4d5d714635d6fadf0f2a234937b92d4d4887bd47
Isvvc <lyons@tuta.io>
2020-09-24 14:17:53 -0600

    Create Xcode project

---
+++ Domain Name Generator/Domain Name Generator/Domain_Name_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Domain_Name_GeneratorApp.swift
+//  Domain Name Generator
+//
+//  Created by Isaac Lyons on 9/24/20.
+//
+
+import SwiftUI
+
+@main
+struct Domain_Name_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Domain Name Generator/Domain Name GeneratorTests/Domain_Name_GeneratorTests.swift
@@ -0,0 +1,33 @@
+//
+//  Domain_Name_GeneratorTests.swift
+//  Domain Name GeneratorTests
+//
+//  Created by Isaac Lyons on 9/24/20.
+//
+
+import XCTest
+@testable import Domain_Name_Generator
+
+class Domain_Name_GeneratorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
